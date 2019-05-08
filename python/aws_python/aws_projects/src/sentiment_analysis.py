9885b1f701d9958b89b2a672c41621e0d1d0e5c3
Susmit Chatterjee <susmit.chatterjee@gmail.com>
2019-05-08 02:12:45 -0500

    Chaging the test string for comprehend

--- python/aws_python/aws_projects/src/sentiment_analysis.py
+++ python/aws_python/aws_projects/src/sentiment_analysis.py
@@ -3,6 +3,6 @@ import json
 
 client = boto3.client('comprehend')
 
-response = client.batch_detect_sentiment(TextList = ['You are a fucking cunt'],LanguageCode = 'en')
+response = client.batch_detect_sentiment(TextList = ['I Love you'],LanguageCode = 'en')
 
 print(response['ResultList'][0]['Sentiment'])
