584598f7dea2c421f2f0c5fd47a689c10f8232db
Susmit Chatterjee <susmit.chatterjee@gmail.com>
2020-05-10 14:45:52 -0500

    remove PI data.

--- python/practice_sandbox/src/twillio_call.py
+++ python/practice_sandbox/src/twillio_call.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client
 
-account_sid = 'AC0f9f34365c17742398fecd6a63e4b5be'
-auth_token = '8ebab73a3575125e1e2a85bc9dba4a40'
+account_sid = 'xxx'
+auth_token = 'xxx'
 
 client = Client(account_sid,auth_token)
 
-phone_list = ['+18035749876']
+phone_list = ['+1123456789']
 
 for pnumber in phone_list:
 	call = client.calls.create(url='https://suschattphp.s3.amazonaws.com/voice.xml',to=pnumber,from_='+100000000',method='GET')
