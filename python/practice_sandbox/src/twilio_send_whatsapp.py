95340aad26c803a9483e068e641fda1fee54115f
Susmit Chatterjee <susmit.chatterjee@gmail.com>
2020-05-10 14:45:09 -0500

    removed token.

--- python/practice_sandbox/src/twilio_send_whatsapp.py
+++ python/practice_sandbox/src/twilio_send_whatsapp.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client 
 from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
  
-account_sid = 'AC0f9f34365c17742398fecd6a63e4b5be' 
-auth_token = '8ebab73a3575125e1e2a85bc9dba4a40' 
+account_sid = 'XXX' 
+auth_token = 'XXX' 
 client = Client(account_sid, auth_token) 
  
 response = MessagingResponse()
